601f1d52ebaf24d816e4cb1539ba3b2647a0a165
samarthaagrawal29 <samarthaagrawal29@mail.com>
2024-06-18 18:32:50 +0530

    All API Completed

--- order/controllers/deleteOrderByID.js
+++ order/controllers/deleteOrderByID.js
@@ -1,5 +1,29 @@
+//Load Model
+require('../orderModel');
+const mongoose = require('mongoose');
+const Order = mongoose.model('Order');
+
 const deleteOrderByID = async (req, res, next) => {
-    
+    const { id } = req.query;
+  if (!id) {
+    res.status(401).send("Order ID is not available!");
+  }
+  try {
+    const response = await Order.findByIdAndDelete(`${id}`);
+    if (response) {
+      res.send("Order Deleted Successfully !!");
+      return next();
+    } else {
+      res
+        .status(401)
+        .send(
+          "Soemthing Went Wrong While Deleting the Order Data Please try again later!!"
+        );
+      return next();
+    }
+  } catch (error) {
+    console.log("Something Wrong In Delete Order By ID Controller");
+  }
 }
 
 module.exports.deleteOrderByID = deleteOrderByID;
